84ac5910acb0a48df85ffd5c21b3f6c952ec1f71
Prashant Dhavale <pndhavale@gmail.com>
2021-12-06 14:09:31 +1100

    Initial Commit
    Telstra Assignment initial commit

---
+++ ToyRobotSimulation.Service/Abstractions/ISimpleCommandProcessorFactory.cs
@@ -0,0 +1,10 @@
+﻿using ToyRobotSimulation.Service;
+using ToyRobotSimulation.Service.Abstractions;
+
+namespace ToyRobotSimulation.Client
+{
+    public interface ISimpleCommandProcessorFactory
+    {
+        ICommandProcessor GetProcessor(CommandType commandType, string[] commands);
+    }
+}
